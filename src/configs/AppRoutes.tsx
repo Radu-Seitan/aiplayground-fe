788f95e0282cf08817f435ec4687aba7fc0d0bf9
Radu-Seitan <raducseitan@gmail.com>
2025-06-05 16:48:15 +0300

    Session 3

--- src/configs/AppRoutes.tsx
+++ src/configs/AppRoutes.tsx
@@ -6,6 +6,7 @@ import { Platforms } from "../components/Platforms";
 import { Scopes } from "../components/Scopes";
 import { Runs } from "../components/Runs";
 import { Prompts } from "../components/Prompts";
+import { ManagePrompt } from "../components/Prompts/ManagePrompt";
 
 export const AppRoutes: FC = () => {
   return (
@@ -15,7 +16,11 @@ export const AppRoutes: FC = () => {
         <Route path={"/platforms"} element={<Platforms />} />
         <Route path={"/scopes"} element={<Scopes />} />
         <Route path={"/runs"} element={<Runs />} />
-        <Route path={"/prompts"} element={<Prompts />} />
+        <Route path={"/prompts"}>
+          <Route index={true} element={<Prompts />} />
+          <Route path={"create"} element={<ManagePrompt />} />
+          <Route path={"view/:id"} element={<ManagePrompt />} />
+        </Route>
         <Route path="*" element={<div>Not found</div>} />
       </Route>
     </Routes>
